858c3bca512a644f31d1bbc16b199b462c40029c
Rodrigo Araujo <rodrigoasilvacc@gmail.com>
2024-06-12 10:23:18 -0300

    First Creation Crud Spring

---
+++ src/test/java/teste/com/example/crudtest/CrudtestApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.com.example.crudtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
